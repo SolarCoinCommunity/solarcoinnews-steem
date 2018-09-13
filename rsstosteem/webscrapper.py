c24321940e5f5b409c3bf22228ea4fadd1f89e5c
JZA <jza@gultab.org>
2018-09-12 20:55:35 -0500

    added new functions

--- rsstosteem/webscrapper.py
+++ rsstosteem/webscrapper.py
@@ -1,7 +1,20 @@
-import bs4
+import config
+import urllib.request as req
+
+from bs4 import BeautifulSoup
 
 from rssparser import buildlist
 
+# Function to scrap the content from the SolarCoin site
 
+def DrupalScrap:
+    for item in buildlist:
+        get_site= req.Request(buildlist[item])
+        with req.urlopen(get_site) as response:
+            site_content = response.read()
+            soup = BeautifulSoup(site_content, 'html.parser')
+            # Position on the article text box
+            article_box= soup.find('div', attrs={'class': 'node__content'})
+return article_box
 
 
