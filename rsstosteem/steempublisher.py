fa4c36a569e15f0948a18b721b95a84b9ed272e3
JZA <jza@gultab.org>
2018-09-14 17:55:34 -0500

    Added some exception handling

--- rsstosteem/steempublisher.py
+++ rsstosteem/steempublisher.py
@@ -1,16 +1,11 @@
 import rsstosteem.config as config
+import rsstosteem.webscrapping
 
 from steem import Steem
 
 # Connect to Solarcoin
 steemaccount=Steem(keys=[config.steemaccount, config.steemkey])
 
-# Format from html to markdown
-# info: https://github.com/gaojiuli/tomd
-from tomd import Tomd
-
-# Filter images and reformat for steemit
-
-
-# Publish document
+Scrap = DrupalScrap()
+Steem.post(article)
 
