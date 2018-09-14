2f949b6dff23e4511ee07d78511e4f4432696b80
JZA <jza@gultab.org>
2018-09-14 17:24:34 -0500

    Added aditional format to the html content

--- rsstosteem/webscrapper.py
+++ rsstosteem/webscrapper.py
@@ -1,9 +1,12 @@
+#!/usr/env python
+
 import rsstosteem.config as config
+from rsstosteem.rssparser import buildlist
+
 import urllib.request as req
 
 from bs4 import BeautifulSoup
-
-from rsstosteem.rssparser import buildlist
+from tmod import Tmod
 
 
 # Function to scrap the content from the SolarCoin site
@@ -18,5 +21,12 @@ def DrupalScrap():
             # Position on the article text box
             for img in soup.find_all('img'):
                 img.decompose()
+            header_box = soup.find('div', attrs={'class': 'page-title'})
             article_box = soup.find('div', attrs={'class': 'node__content'})
-    return article_box
+            article_markdown = Tmod(article_box).markdown
+            header_box = Tmod(header_box).markdown
+            permalink_nospace =header_box.replace(" ", "_")
+            permalink = permalink_nospace.lowercase()
+            article = (header_box, article_box, permalink)
+    return article
+
